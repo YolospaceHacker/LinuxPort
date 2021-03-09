3dadd44475d1cd3b4f1916766272aff3fad51aee
ZenZenVibe <13644560+jossets@users.noreply.github.com>
2021-03-09 10:57:10 +0100

    Initial commit

---
+++ WindowsApps/CSharpForms/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace CSharpForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.bClose = new System.Windows.Forms.Button();
+            this.rtbLog = new System.Windows.Forms.RichTextBox();
+            this.bListDlls = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hello yolo";
+            // 
+            // bClose
+            // 
+            this.bClose.Location = new System.Drawing.Point(264, 263);
+            this.bClose.Name = "bClose";
+            this.bClose.Size = new System.Drawing.Size(144, 41);
+            this.bClose.TabIndex = 1;
+            this.bClose.Text = "Close";
+            this.bClose.UseVisualStyleBackColor = true;
+            this.bClose.Click += new System.EventHandler(this.bClose_Click);
+            // 
+            // rtbLog
+            // 
+            this.rtbLog.Location = new System.Drawing.Point(16, 44);
+            this.rtbLog.Name = "rtbLog";
+            this.rtbLog.Size = new System.Drawing.Size(391, 203);
+            this.rtbLog.TabIndex = 2;
+            this.rtbLog.Text = "";
+            this.rtbLog.WordWrap = false;
+            // 
+            // bListDlls
+            // 
+            this.bListDlls.Location = new System.Drawing.Point(16, 263);
+            this.bListDlls.Name = "bListDlls";
+            this.bListDlls.Size = new System.Drawing.Size(142, 41);
+            this.bListDlls.TabIndex = 3;
+            this.bListDlls.Text = "List Dlls";
+            this.bListDlls.UseVisualStyleBackColor = true;
+            this.bListDlls.Click += new System.EventHandler(this.bListDlls_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 316);
+            this.Controls.Add(this.bListDlls);
+            this.Controls.Add(this.rtbLog);
+            this.Controls.Add(this.bClose);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bClose;
+        private System.Windows.Forms.RichTextBox rtbLog;
+        private System.Windows.Forms.Button bListDlls;
+    }
+}
+
---
+++ WindowsApps/CSharpForms/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpForms
+{
+    public partial class Form1 : Form
+    {
+        string appDir = "";
+        public Form1()
+        {
+            InitializeComponent();
+            //
+            // Set App directory based on exe path & remove /bin/
+            string binDir = AppDomain.CurrentDomain.BaseDirectory; //Environment.CurrentDirectory;
+            DirectoryInfo parentDir = Directory.GetParent(binDir);
+            appDir = parentDir.Parent.FullName;
+        }
+
+        void TT(string txt)
+        {
+            rtbLog.Text += txt;
+        }
+
+        public void  DumpLoadedDllList()
+        {
+            // List loaded dlls
+            Process[] processes = null;
+            int processID;
+    
+            Console.WriteLine("[+] Dump loaded Dlld\n");
+            processID = Process.GetCurrentProcess().Id;
+            processes = Process.GetProcesses();
+            foreach (Process process in processes)
+            {
+                if (process.Id == processID)
+                {
+                    TT("PID:  " + process.Id+"\n");
+                    TT("Name: " + process.ProcessName+"\n");
+                    TT("Modules: ");
+
+                    foreach (ProcessModule module in process.Modules)
+                    {
+                        TT(module.FileName+"\n");
+                    }
+                    TT("======\n\n");
+                }
+            }
+            TT("\n");
+
+        }
+
+        [DllImport("kernel32.dll")]
+        static extern bool LoadLibraryA(string hModule);
+
+        void PreLoadDlls()
+        {
+            // Pre load Dlls
+            List<string> Dlls = new List<string>()
+                {
+                    "CefSharp.Core.dll",
+                    "cfgmgr32.dll",
+                    "chrome_elf.dll",
+                    "coloradapterclient.dll",
+                    "comdlg32.dll",
+                    "credui.dll",
+                    "cryptui.dll",
+                    "d3d9.dll",
+                    "dbghelp.dll",
+                    "devobj.dll",
+                    "dwmapi.dll",
+                    "dxva2.dll",
+                    "hid.dll",
+                    "icm32.dll",
+                    "iertutil.dll",
+                    "libcef.dll",
+                    "MMDevAPI.dll",
+                    "mscms.dll",
+                    "msvcp140.dll",
+                    "netapi32.dll",
+                    "netutils.dll",
+                    "nlaapi.dll",
+                    "oleacc.dll",
+                    "propsys.dll",
+                    "samcli.dll",
+                    "urlmon.dll",
+                    "vcruntime140.dll",
+                    "winmm.dll",
+                    "winspool.drv",
+                    "winsta.dll",
+                    "wtsapi32.dll",
+                };
+            var files = Directory.EnumerateFiles(appDir + @"\linux\dll\", "*.dll", SearchOption.AllDirectories);
+            foreach (string dll in files)
+            {
+                //var DLL = Assembly.LoadFile(dll);
+                TT("Dll preload: " + dll);
+                LoadLibraryA(dll);
+            }
+            TT("Dll preload: Done");
+        }
+        private void bClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void bListDlls_Click(object sender, EventArgs e)
+        {
+            DumpLoadedDllList();
+        }
+    }
+}
